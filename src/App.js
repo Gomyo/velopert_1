b5ed3751e60a9de73769f15db11c889c052d52e4
최준원 <cjw1102@naver.com>
2021-10-20 17:26:26 +0900

    ~ 5장

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import EventPractice from './EventPracticeFunc'
+import React, { Component } from 'react'
+import A from './ScrollBox'
 
-const App = () => {
-  return <EventPractice />
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <A ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App
