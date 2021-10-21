d17da02cac1484244420c067e501ca79a379dae6
최준원 <cjw1102@naver.com>
2021-10-21 14:35:39 +0900

    ~ 8장

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
-import A from './IterationSample'
+import A from './LifeCycleSample'
+import ErrorBoundary from './ErrorBoundary'
+
+function getRanCol() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRanCol(),
+    })
+  }
   render() {
-    return <A />
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <A color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    )
   }
 }
 
